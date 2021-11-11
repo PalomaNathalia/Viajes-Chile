6122a8bafc792e49461e663fd2008cc769cb824f
Paloma <pcuellomaidana@gmail.com>
2021-11-11 20:29:39 -0300

    Alert de Bienvenida

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+//Bienvenida
+var nombre= prompt("Hola, ¿Cuál es tu nombre?", "Nombre");
+alert("Bienvenido a Viajes Chile " + nombre);
+
+//Tooltip para iconos en footer
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
