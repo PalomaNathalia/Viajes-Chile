4f7eb0a8f61cfbc94ae1fd7d6e011e54d1435c71
Paloma <pcuellomaidana@gmail.com>
2021-11-15 21:04:45 -0300

    Ajutes de tamaños

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,27 @@
 //Bienvenida
+/*
 var nombre= prompt("Hola, ¿Cuál es tu nombre?", "Nombre");
 alert("Bienvenido a Viajes Chile " + nombre);
+
+*/
 
 //Tooltip para iconos en footer
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
+  $("#tarjeta1").on("click",function(){
+    $("#target1").toggle();
+  });
+
+  $("#tarjeta2").on("click",function(){
+    $("#target2").toggle();
+  });
+
+  $("#tarjeta3").on("click",function(){
+    $("#target3").toggle();
+  });
+
+  $("#tarjeta4").on("click",function(){
+    $("#target4").toggle();
+  });
