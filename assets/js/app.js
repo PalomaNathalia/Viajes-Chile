bf078f569ed5b580ddc0bbd559ff4939f08ac221
Paloma <pcuellomaidana@gmail.com>
2021-11-16 17:44:17 -0300

    Activé saludo de bienvenida

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
 //Bienvenida
-/*
+
 var nombre= prompt("Hola, ¿Cuál es tu nombre?", "Nombre");
 alert("Bienvenido a Viajes Chile " + nombre);
 
-*/
 
 //Tooltip para iconos en footer
 $(function () {
